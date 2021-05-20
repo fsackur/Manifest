93eda7f9ed163aa1c70ed7becf4457eeaf0fbfa8
Freddie Sackur <github@dustyfox.uk>
2021-05-20 21:32:54 +0100

    Use recursion better, support string/array/hashtable ASTs

--- Private/ConvertFrom-ExpressionAst.ps1
+++ Private/ConvertFrom-ExpressionAst.ps1
@@ -2,40 +2,64 @@ function ConvertFrom-ExpressionAst
 {
     param
     (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [AllowNull()]
         [System.Management.Automation.Language.ExpressionAst]$ExpressionAst
     )
 
-    $Output = [ordered]@{}
 
-    $KvpTuples = $ExpressionAst.KeyValuePairs
-    foreach ($KvpTuple in $KvpTuples)
+    process
     {
-        $KeyAst  = $KvpTuple.Item1
-        $Key     = $KeyAst.Value       # We know it's a BareWord
+        if ($null -eq $ExpressionAst)
+        {
+            return
+        }
 
-        $ValAst  = $KvpTuple.Item2
-        $ExprAst = $ValAst.PipelineElements.Expression    # We know value will always have this
-
-        switch ($ExprAst.GetType())
+        switch ($ExpressionAst.GetType())
         {
             ([System.Management.Automation.Language.StringConstantExpressionAst])
             {
-                $Output.Add($Key, $ExprAst.Value)
+                return $ExpressionAst.Value
+            }
+
+            ([System.Management.Automation.Language.ArrayLiteralAst])
+            {
+                Write-Output @($ExpressionAst.Elements | ConvertFrom-ExpressionAst) -NoEnumerate
+                return
             }
 
             ([System.Management.Automation.Language.ArrayExpressionAst])
             {
-                $ElementAsts = $ExprAst.SubExpression.Statements.PipelineElements.Expression.Elements
-                $Output.Add($Key, $ElementAsts.Value)
+                $SubExprAst = $ExpressionAst.SubExpression.Statements.PipelineElements.Expression
+                Write-Output @($SubExprAst | ConvertFrom-ExpressionAst) -NoEnumerate
+                return
             }
 
             ([System.Management.Automation.Language.HashtableAst])
             {
-                $Output.Add($Key, (ConvertFrom-ExpressionAst $ExprAst))
+                $Output = [ordered]@{}
+
+                $KvpTuples = $ExpressionAst.KeyValuePairs
+                foreach ($KvpTuple in $KvpTuples)
+                {
+                    $KeyAst  = $KvpTuple.Item1
+                    $Key     = $KeyAst.Value       # We know it's a BareWord
+
+                    $_ValAst = $KvpTuple.Item2
+                    $ExprAst = $_ValAst.PipelineElements.Expression    # We know value will always have this
+                    $_Value  = $ExprAst | ConvertFrom-ExpressionAst
+
+                    $Output.Add($Key, $_Value)
+                }
+
+                return $Output
+            }
+
+            default
+            {
+                $Exception = [System.NotImplementedException]::new("AST type not supported: $_")
+                Write-Error -Exception $Exception -TargetObject $_ -ErrorAction Stop
             }
         }
     }
-
-    return $Output
 }
